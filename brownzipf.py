9d6401ddd01c4d88c9bc0e3a2b58d7857934431d
Alex Nelson <alexander.nelson@nist.gov>
2015-04-28 15:54:46 -0400

    Implement minimal changes to run in Python 3
    These changes in the script can run in Python 2.7 and Python 3.4 with
    NTLK as provided by MacPorts (current NLTK version: 3.0.2).
    
    Unfortunately, Python 3 does not generate the same point labels at the
    right tail of the graph.  Repeated runs in Python 2 end with "jawbone";
    repeated runs in Python 3 end with random tokens from the tail.

--- brownzipf.py
+++ brownzipf.py
@@ -1,15 +1,22 @@
 from __future__ import division
+import sys
 from itertools import *
 from pylab import *
 from nltk.corpus import brown
-from string import lower
 from collections import Counter
 
+# Pick a lowercasing function according to Python version
+if sys.version_info.major < 3:
+    from string import lower
+else:
+    lower = str.lower
 
 # The data: token counts from the Brown corpus
-tokens_with_count = Counter(imap(lower, brown.words()))
+tokens_with_count = Counter(map(lower, brown.words()))
+#Dictionary keys in Python 3 aren't indexable, so store the tokens as a list.
+#The list is sorted to prevent changes in display text on repeated runs.
 counts = array(tokens_with_count.values())
-tokens = tokens_with_count.keys()
+tokens = [k for k in tokens_with_count.keys()]
 
 # A Zipf plot
 ranks = arange(1, len(counts)+1)
